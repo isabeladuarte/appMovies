766d2bb4bdbdf9e890c3f1e53bee1cc8878fb121
Isabela Duarte <isabeladuartelbg@gmail.com>
2024-05-31 13:59:30 -0300

    Add project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>MaiaFlix &copy; Desenvolvido por Isabela Duarte 2024</h2>
+        </footer>
+    );
+}
+
+export default Footer;
+
