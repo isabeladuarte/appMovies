475c874b2063c29cbb2f0c52026a304beaf06c03
Isabela Duarte <isabeladuartelbg@gmail.com>
2024-06-04 23:16:33 -0300

    Create component category

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia"
+]
+
+function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
     <>
       <Header />
       <Banner image="home" />
-      <Container> 
-        <h2>Geografia</h2>
-        <section className="cards">
-          {
-            videos.map(video => 
-              <Card id={video.id} key={video.id}/>
-            )
-          }
-        </section>
+      <Container>
+        <Category category="Geografia">
+        {
+          filterCategory(0).map(video => 
+            <Card id={video.id} key={video.id}/>
+          )
+        }
+        </Category>
+        
+        <Category category="Como fazer e usar">
+        {
+          filterCategory(1).map(video => 
+            <Card id={video.id} key={video.id}/>
+          )
+        }
+        </Category>
+
+        <Category category="Astronomia e Geografia">
+        {
+          filterCategory(2).map(video => 
+            <Card id={video.id} key={video.id}/>
+          )
+        }
+        </Category>
+
+        <Category category="Climatologia, Meteorologia, Vegetação">
+        {
+          filterCategory(3).map(video => 
+            <Card id={video.id} key={video.id}/>
+          )
+        }
+        </Category>
+
+        <Category category="Geologia e Hidrografia">
+        {
+          filterCategory(4).map(video => 
+            <Card id={video.id} key={video.id}/>
+          )
+        }
+        </Category>
+
       </Container>
       <Footer />
     </>
