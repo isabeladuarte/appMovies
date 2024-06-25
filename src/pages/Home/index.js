ed787fe56fa633d91b32ed01de66b1cf597c01e8
Isabela Duarte <isabeladuartelbg@gmail.com>
2024-06-25 17:51:13 -0300

    Create hook and useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, { categories, filterCategory } from "../../components/Category";
@@ -6,18 +7,34 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function Home() {
+  function pegarNome(event) {
+    console.log(event.target.value)
+    setNome(event.target.value)
+  }
+
+  //Hook = gancho
+  const [ nome, setNome ] = useState()
+
   return (
     <>
       <Header />
       <Banner image="home" />
       <Container>
-        {categories.map((category, index) => 
+
+        <input 
+          text="text"
+          placeholder="Nome"
+          onChange={pegarNome}
+        />
+
+        <h2>{nome}</h2>
+        {/* {categories.map((category, index) => 
           <Category category={category}>
             {filterCategory(index).map(video => 
               <Card id={video.id} key={video.id}/>
             )}
           </Category>
-        )}
+        )} */}
       </Container>
       <Footer />
     </>
