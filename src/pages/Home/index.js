0908d06a9bba5e9a9cebdbeaf713576165482fd7
Isabela Duarte <isabeladuartelbg@gmail.com>
2024-07-01 16:31:12 -0300

    Add component searchVideoList and search videos for category or title

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, { categories, filterCategory } from "../../components/Category";
@@ -7,34 +6,18 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function Home() {
-  function pegarNome(event) {
-    console.log(event.target.value)
-    setNome(event.target.value)
-  }
-
-  //Hook = gancho
-  const [ nome, setNome ] = useState()
-
   return (
     <>
       <Header />
       <Banner image="home" />
       <Container>
-
-        <input 
-          text="text"
-          placeholder="Nome"
-          onChange={pegarNome}
-        />
-
-        <h2>{nome}</h2>
-        {/* {categories.map((category, index) => 
+        {categories.map((category, index) => 
           <Category category={category}>
             {filterCategory(index).map(video => 
               <Card id={video.id} key={video.id}/>
             )}
           </Category>
-        )} */}
+        )}
       </Container>
       <Footer />
     </>
