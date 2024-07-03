0866ff9c20adfc284c43c3cc0b68caa81ac705ef
Isabela Duarte <isabeladuartelbg@gmail.com>
2024-07-03 16:26:33 -0300

    Create page favorites and route

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
+import iconFavorite from "./favorite.png";
+import iconUnFavorite from "./unfavorite.png";
 
 function Card({ id }) {
-    return(
+    return (
         <section className={styles.card}>
             <Link to={`/watch/${id}`}>
                 <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} 
-                alt="Capa" />
+                alt="Capa" class={styles.cover} />
             </Link>
+            <figure className={styles.icon}>
+                <img src={iconFavorite} alt="Ícone" />
+            </figure>
         </section>
     );
 }
